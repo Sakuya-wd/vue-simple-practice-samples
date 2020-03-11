57d7cd445cc6d771dbcecc36e45f542ad7233c00
Riin Fruit <nassii74@gmail.com>
2020-03-11 22:31:48 +0900

    feat: 進捗を反映

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,78 @@
 <template>
   <div id="app">
-    <h1>{{ message }}</h1>
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <hello-component></hello-component>
+    <p>
+      <input type="text" v-model="message">
+    </p>
+    <p>
+      <input type="text" v-model="message">
+    </p>
+    <ul>
+      <li v-for="value in user">{{ value }}</li>
+    </ul>
+    <hr>
+    <ul>
+      <li v-for="(value,key) in user">{{ key }} : {{ value }}</li>
+    </ul>
+    <hr>
+    <ul>
+      <li v-for="(v,k) in user">{{ k }} : {{ v }}</li>
+    </ul>
+    <ol>
+      <li v-for="color in colors">{{ color }}</li>
+    </ol>
+    <p v-show="toggle">
+      <!--v-showはDOMレベルでは削除されない、cssのdisply:noneを操作している-->
+      Hello(v-show)
+    </p>
+
+    <p>
+      {{ message }}
+    </p>
+    <p>
+      {{ count }}
+    </p>
+    <p>
+      {{ user.prefecture }}
+    </p>
+    <p>
+      {{ colors[1] }}
+    </p>
+    <button v-on:click="onclick">クリック</button>
+    <h1>
+      {{ now }}
+    </h1>
   </div>
 </template>
 
 <script>
+import HelloComponent from "./components/HelloComponent"
 
 export default {
-  data(){
+  components: {
+    HelloComponent
+  },
+  data:function(){
     return {
-      message: "Hello Vue!"
+      now: '',
+      toggle: true,
+      message: 'Hello Vue.js!',
+      count: 10,
+      user: {
+        lastName: 'Yamada',
+        firdtName: 'taro',
+        prefecture: 'Tokyo',
+      },
+      colors: ['Red','Green','Blue'],
+    }
+  },
+  methods: {
+    onclick: function(){
+      this.now = new Date().toLocaleString();
     }
   }
+  //データバインディング（データと描画を同期）
+  //ディレクティブ（v-で始まるもの、日本語で指示という意味、Vue.jsになんらかの指示をだす仕組み）
 };
 </script>
 
---
+++ src/components/HelloComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>hello-component</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        // このnameはなくて大丈夫
+        name: "HelloComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
